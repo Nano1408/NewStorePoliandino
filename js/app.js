60596ca50c93ef9df8fb4a14d123fdb3a9502c09
Nano1408 <fernandowjose2000@gmail.com>
2022-11-07 20:05:22 -0500

    NewStorePoliandino

---
+++ js/app.js
@@ -0,0 +1,109 @@
+const carrito = document.querySelector("#carrito");
+const vaciarcarrito = document.querySelector("#vaciar-carrito");
+const divCarrito = document.querySelector("#lista-carrito tbody");
+const listaArticulos = document.querySelector("#lista-articulos");
+
+let articulosCarrito = [];
+
+registrarEventListeners()
+
+//----------------------FUNCIONES---------------------------
+//CAPTURAMOS LOS EVENTOS CON ESTA FUNCION
+function registrarEventListeners(){
+    listaArticulos.addEventListener("click", agregarArticulo);
+    //eleiminar todos los articulos (vaciar todo)
+    carrito.addEventListener("click", eliminarArticulo);
+    vaciarcarrito.addEventListener("click", ()=>{
+        articulosCarrito = [];
+        limpiarHTML();
+    });
+}
+//AGREGAMOS LOS ARTICULOS AL CARRITO
+function agregarArticulo(e){
+    e.preventDefault()//previene que el boton precargue la pagina
+    if(e.target.classList.contains("agregar-carrito")){
+        const articulosSeleccionados = e.target.parentElement.parentElement;
+        leerDatosArticulos(articulosSeleccionados);
+    }
+}
+
+//funcion para eliminar articulos
+function eliminarArticulo(e){
+    if(e.target.classList.contains("borrar-articulo")){
+        const articuloID = e.target.getAttribute("data-id");
+        articulosCarrito = articulosCarrito.filter(articulo=>articulo.id !== articuloID);
+        carritoHTML();
+        // console.log(e.target.getAttribute("data-id"))
+    }
+}
+
+//FUNCION QUE LEA EL HTML DONDE SE DA CLICK Y LO EXTRAEMOS
+function leerDatosArticulos(articulo){
+    //creemos un objeto con los datos de card
+    const infoArticuloimg = {
+        imagen:articulo.querySelector(".card img").src,
+        titulo:articulo.querySelector("h4").textContent,
+        precio:articulo.querySelector(".precio span").textContent,
+        id:articulo.querySelector("a").getAttribute("data-id"),
+        cantidad:1,
+    }
+    //revisemos si el articulo ya esta en el array y si esta, solo actualizo la cantidad
+    //sino adiciono el nuevo articulo
+    const existeArticulo = articulosCarrito.some(articulo => articulo.id === infoArticuloimg.id);
+    if(existeArticulo){
+        const articulo = articulosCarrito.map(articulo=>{
+            if(articulo.id === infoArticuloimg.id){
+                articulo.cantidad++;
+                return articulo;
+            }else{
+                return articulo;
+            }
+        })
+        articulosCarrito =[...articulo];
+    }else{
+        //sino existe agregamos el producto al carrito
+        articulosCarrito = [...articulosCarrito,infoArticuloimg];
+    }
+    carritoHTML();
+}
+
+//INYECTAR LOS ARTICULOS DEL ARRAY EN EL HTML DEL CARRITO (tbody)
+function carritoHTML(){
+    //llama la funcion que limpia el carrito
+    limpiarHTML();
+    articulosCarrito.forEach((articulo)=>{
+        const {imagen,titulo,precio,cantidad,id} = articulo;//destructuring
+
+       const row = document.createElement("tr");
+       row.innerHTML = `
+       <td>
+       <img src="${articulo.imagen}" width="100%"">
+       </td>
+        <td>
+        ${titulo}
+        </td>
+        <td>
+        ${precio}
+        </td>
+        <td>
+        ${cantidad}
+        </td>
+        <td>
+        <a href="#" class="borrar-articulo" data-id="${articulo.id}"><img class="borrar-articulo" src="./img/eliminar-png.png" alt="eliminar.png" data-id="${articulo.id}" width="30px""></a>
+        </td>
+        `;
+        console.log(row)
+        divCarrito.appendChild(row);
+    })
+}
+
+//crear una funcion que nos limpie el html del carrito cuando demos click
+function limpiarHTML(){
+    //esta forma es lenta y sirve para aplicativos pequeños
+    // divCarrito.innerHTML = "";
+
+    //esta forma es mucho mejor para limpiar el html
+    while(divCarrito.firstChild){
+        divCarrito.removeChild(divCarrito.firstChild);
+    }
+}
